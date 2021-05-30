4d62f4532ef1c86ac3e47ea54e06a0fcce5bc99e
lee sang hun <oper6909@gmail.com>
2021-05-31 01:27:54 +0900

    feat: very simple todo

--- app/src/Todo.js
+++ app/src/Todo.js
@@ -1,4 +1,15 @@
 import axios from 'axios';
 import React from 'react';
 
-const Todo = (props) => {};
+const Todo = ({ title, date, description, user }) => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ width: '500px' }}>
+        <p>
+          <span style={{ fontWeight: 'bold' }}>{title}</span>
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+};

daf7a1f56c7198127a7b7d2bcb943f274bbfe495
lee sang hun <oper6909@gmail.com>
2021-06-07 00:13:10 +0900

    feat: apply sqlalchemy async session

--- server/crud.py
+++ server/crud.py
@@ -1,29 +1,38 @@
-from sqlalchemy.orm import Session
+import sqlalchemy as sa
+from typing import List
+
+# from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
 import models, schemas
 
 
-def get_user_by_email(db: Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
+async def get_all_user(db: AsyncSession) -> List[models.User]:
+    stmt = select(models.User)
+    query = await db.scalar(stmt)
+    return query
 
 
-def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(email=user.email,
-                          hashed_password=fake_hashed_password)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+# def get_user_by_email(db: Database, email: str):
+#     # query = sa.select()
+#     return db.query(models.User).filter(models.User.email == email).first()
 
+# def create_user(db: Database, user: schemas.UserCreate):
+#     fake_hashed_password = user.password + "notreallyhashed"
+#     db_user = models.User(email=user.email,
+#                           hashed_password=fake_hashed_password)
+#     db.add(db_user)
+#     db.commit()
+#     db.refresh(db_user)
+#     return db_user
 
-def get_todos(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Todo).offset(skip).limit(limit).all()
+# def get_todos(db: Database, skip: int = 0, limit: int = 100):
+#     return db.query(models.Todo).offset(skip).limit(limit).all()
 
-
-def create_user_todo(db: Session, todo: schemas.TodoCreate, user_id: int):
-    db_todo = models.Todo(**todo.dict(), owner_id=user_id)
-    db.add(db_todo)
-    db.commit()
-    db.refresh(db_todo)
-    return db_todo
+# def create_user_todo(db: Database, todo: schemas.TodoCreate, user_id: int):
+#     db_todo = models.Todo(**todo.dict(), owner_id=user_id)
+#     db.add(db_todo)
+#     db.commit()
+#     db.refresh(db_todo)
+#     return db_todo
--- server/database.py
+++ server/database.py
@@ -1,17 +1,21 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(
+Base = declarative_base()
+
+engine = create_async_engine(
 
     # The argument below is needed only for SQLite,
     # since SQLite will only allow one thread to communicate with it.
     # https://fastapi.tiangolo.com/tutorial/sql-databases/#note
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    echo=True,
+)
 
-Base = declarative_base()
+# expire_on_commit=False will prevent attributes from being expired
+# after commit.
+# create AsyncSession with expire_on_commit=False
+AsyncSessionLocal = AsyncSession(engine, expire_on_commit=False)
